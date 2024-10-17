daaf5d0c41d14742522d3387d674b28df8bcd62f
baam <dlgyqja1234@naver.com>
2024-10-17 11:13:28 +0900

    modify: 영화리스트 영화포스터 출력완료(#12)

--- src/pages/movie/MovieDetailPage.jsx
+++ src/pages/movie/MovieDetailPage.jsx
@@ -1,107 +1,74 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { fetchMovieById } from "../../api/movie/movieApi"; // fetchMovieById로 수정
+import { Link, useParams } from "react-router-dom";
+import { getMovieList } from "../../api/movieApi";
 
 const MovieDetailPage = () => {
-  const [movies, setMovies] = useState(null); // 영화 정보 상태 관리
-  const [activeTab, setActiveTab] = useState("details"); // 기본 탭을 'details'로 설정
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태
-  const [hasPurchased, setHasPurchased] = useState(false); // 구매 여부
-  const navigate = useNavigate(); // useNavigate 훅 사용
-  const { id } = useParams(); // URL에서 id 가져오기
-
-  const handleWriteReview = () => {
-    if (!isLoggedIn) {
-      alert("로그인 후에 관람평을 작성할 수 있습니다.");
-    } else if (!hasPurchased) {
-      alert("구매 후에 관람평을 작성할 수 있습니다.");
-    } else {
-      navigate("/write-review");
-    }
-  };
-
-  useEffect(() => {
-    const loadMovieDetails = async () => {
-      try {
-        const data = await fetchMovieById(id); // fetchMovieById로 변경
-        setMovies(data);
-      } catch (error) {
-        console.error("영화 정보를 가져오는 데 오류가 발생했습니다:", error);
-      }
-    };
-    loadMovieDetails();
-  }, [id]);
+  const [movie, setMovie] = useState([]);
+  const [activeTab, setActiveTab] = useState("details");
 
   return (
     <>
       <section className="container mx-auto p-6">
-        {movies ? ( // movies가 null이 아닐 때만 렌더링
-          <div key={movies.movieNum} className="flex">
-            <img
-              src={movies.posterUrl} // 포스터 URL을 가져옵니다.
-              className="mt-4 rounded-md"
-              alt={`${movies.korTitle} 포스터`}
-            />
-            {/* 영화 정보 */}
-            <div className="ml-10 mt-3 flex-1">
-              <span className="mb-5 block text-4xl font-bold">
-                {movies.korTitle}
+        <div className="flex">
+          <img
+            src={`http://localhost:8080/api/admin/movie/view/${movie.posterUrl}`} // API에서 가져온 포스터 URL 사용
+            className="mt-4 rounded-md"
+            alt={`${movie.korTitle} 포스터`}
+          />
+          <div className="ml-10 mt-3 flex-1">
+            <span className="mb-5 block text-4xl font-bold">
+              {movie.korTitle}
+            </span>
+            <div className="mb-10 flex space-x-2 font-medium text-gray-400">
+              <span className="border-r-4 border-r-gray-200 pr-4 text-2xl">
+                {movie.movieStartDate} 개봉
               </span>
-              <div className="mb-10 flex space-x-2 font-medium text-gray-400">
-                <span className="border-r-4 border-r-gray-200 pr-4 text-2xl">
-                  {movies.movieStartDate} 개봉
-                </span>
-                <span className="border-r-4 border-r-gray-200 pr-4 text-2xl">
-                  {movies.runTime}분
-                </span>
-                <span className="text-2xl">{movies.rating}</span>
-              </div>
-
-              <p className="my-4 mb-6 max-h-48 overflow-y-auto text-2xl text-gray-200">
-                {movies.desc}
-              </p>
-              <Link
-                to={`/reservation/${movies.movieNum}`}
-                className="rounded-md bg-gray-400 px-24 py-5 text-2xl text-black"
-              >
-                예매하기
-              </Link>
+              <span className="border-r-4 border-r-gray-200 pr-4 text-2xl">
+                {movie.runTime}분
+              </span>
+              <span className="text-2xl">{movie.rating}</span>
             </div>
+
+            <p className="my-4 mb-6 max-h-48 overflow-y-auto text-2xl text-gray-200">
+              {movie.movieDesc}
+            </p>
+            <Link
+              to={`/reservation/${movie.movieNum}`}
+              className="rounded-md bg-emerald-500 px-24 py-5 text-2xl text-white"
+            >
+              예매하기
+            </Link>
           </div>
-        ) : (
-          <div className="ml-10 mt-3 flex-1 text-2xl">
-            Loading movie details...
-          </div>
-        )}
+        </div>
       </section>
 
       <section className="pb-40 pt-20 text-xl">
         <div className="flex justify-center space-x-4 text-2xl">
           <button
-            className={`rounded-md p-2 transition-all duration-300 ${
+            className={`rounded-md p-2 ${
               activeTab === "details"
-                ? "bg-gray-700 font-bold text-white"
-                : "bg-gray-300 text-black hover:bg-gray-400"
+                ? "bg-gray-700 text-white"
+                : "bg-gray-300 text-black"
             }`}
             onClick={() => setActiveTab("details")}
           >
             상세정보
           </button>
           <button
-            className={`rounded-md p-2 transition-all duration-300 ${
+            className={`rounded-md p-2 ${
               activeTab === "reviews"
-                ? "bg-gray-700 font-bold text-white"
-                : "bg-gray-300 text-black hover:bg-gray-400"
+                ? "bg-gray-700 text-white"
+                : "bg-gray-300 text-black"
             }`}
             onClick={() => setActiveTab("reviews")}
           >
             관람평
           </button>
           <button
-            className={`rounded-md p-2 transition-all duration-300 ${
+            className={`rounded-md p-2 ${
               activeTab === "trailer"
-                ? "bg-gray-700 font-bold text-white"
-                : "bg-gray-300 text-black hover:bg-gray-400"
+                ? "bg-gray-700 text-white"
+                : "bg-gray-300 text-black"
             }`}
             onClick={() => setActiveTab("trailer")}
           >
@@ -109,57 +76,32 @@ const MovieDetailPage = () => {
           </button>
         </div>
 
-        <div className="mt-4 text-xl" key={movies?.movieNum}>
-          {movies ? ( // movies가 null이 아닐 때만 렌더링
-            activeTab === "details" && (
-              <ul>
-                <li className="mb-10 mt-20 text-3xl">영화정보</li>
-                <li className="mb-5 ml-6 list-disc">
-                  <span className="text- mr-5">장르 :</span> {movies.genre}
-                </li>
-                <li className="mb-5 ml-6 list-disc">
-                  <span className="mr-5">감독 :</span>
-                  {movies.director}
-                </li>
-                <li className="mb-5 ml-6 list-disc">
-                  <span className="mr-5">출연 :</span>
-                  {movies.cast}
-                </li>
-              </ul>
-            )
-          ) : (
-            <div className="ml-10 mt-3 flex-1 text-2xl">
-              Loading movie details...
+        <div className="mt-4 text-xl">
+          {activeTab === "details" && (
+            <ul>
+              <li className="mb-10 mt-20 text-3xl">영화정보</li>
+              <li className="mb-5 ml-6 list-disc">
+                <span className="mr-5">장르 :</span> {movie.genre}
+              </li>
+              <li className="mb-5 ml-6 list-disc">
+                <span className="mr-5">감독 :</span> {movie.director}
+              </li>
+              <li className="mb-5 ml-6 list-disc">
+                <span className="mr-5">출연 :</span> {movie.cast}
+              </li>
+            </ul>
+          )}
+
+          {activeTab === "reviews" && <div>리뷰 영역</div>}
+
+          {activeTab === "trailer" && movie.trailerUrl && (
+            <div className="mb-5 mt-20 text-3xl">
+              <video className="h-auto w-full" controls>
+                <source src={movie.trailerUrl} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
             </div>
           )}
-          {activeTab === "reviews" && (
-            <ol>
-              <li className="mb-5 mt-20 flex justify-between border p-4">
-                <div className="text-2xl">
-                  (영화명) 재미있게 보셨나요? 영화의 어떤점이 좋았는지
-                  이야기해주세요.
-                </div>
-                <div
-                  className="cursor-pointer p-1 text-xl"
-                  onClick={handleWriteReview}
-                >
-                  관람평 쓰기
-                </div>
-              </li>
-              <li>관람평 1</li>
-              <li>관람평 2</li>
-              <li>관람평 3</li>
-            </ol>
-          )}
-          {activeTab === "trailer" &&
-            movies?.trailerUrl && ( // trailerUrl이 있을 때만 렌더링
-              <div className="mb-5 mt-20 text-3xl">
-                <video className="h-auto w-full" controls>
-                  <source src={movies.trailerUrl} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
-              </div>
-            )}
         </div>
       </section>
     </>
--- src/pages/movie/MovieListPage.jsx
+++ src/pages/movie/MovieListPage.jsx
@@ -1,23 +1,20 @@
-import { Link, useSearchParams } from "react-router-dom";
-import { fetchMovies } from "../../api/movie/movieApi";
+import { Link } from "react-router-dom";
+import { getMovieList } from "../../api/movieApi";
 import { useEffect, useState, useRef, useCallback } from "react";
 
 const MovieListPage = () => {
-  const [movies, setMovies] = useState([]);
-  const [page, setPage] = useState(1); // 현재 페이지
-  const size = 10; // 한 페이지에 보여줄 영화 수
-  const [hasMore, setHasMore] = useState(true); // 더 불러올 영화가 있는지 확인
-
-  // 마지막 요소 참조를 위한 ref
+  const [page, setPage] = useState(1);
+  const size = 10;
+  const [hasMore, setHasMore] = useState(true);
+  const [movie, setMovie] = useState([]); // 초기값을 빈 배열로 설정
   const observer = useRef();
 
-  // 무한 스크롤 핸들링 함수
   const lastMovieElementRef = useCallback(
     (node) => {
       if (observer.current) observer.current.disconnect();
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting && hasMore) {
-          setPage((prevPage) => prevPage + 1); // 페이지 증가
+          setPage((prevPage) => prevPage + 1);
         }
       });
       if (node) observer.current.observe(node);
@@ -28,11 +25,11 @@ const MovieListPage = () => {
   useEffect(() => {
     const loadMovies = async () => {
       try {
-        const data = await fetchMovies(page, size); // 현재 페이지와 크기를 전달
-        setMovies((prevMovies) => [...prevMovies, ...data.content]); // 이전 데이터에 추가
-        setHasMore(data.content.length === size); // 더 가져올 영화가 있는지 확인
+        const data = await getMovieList(page, size);
+        setMovie((prevMovies) => [...prevMovies, ...data.content]); // 배열 업데이트
+        setHasMore(data.content.length === size);
       } catch (error) {
-        console.error("영화를 가져오는 데 오류가 발생했습니다:", error);
+        console.error("Error fetching movies:", error);
       }
     };
 
@@ -43,89 +40,43 @@ const MovieListPage = () => {
     <div className="py-4 pb-36 pt-10">
       <h1 className="pb-20 pl-2 text-left text-2xl text-white">전체 영화</h1>
       <div className="container mx-auto grid grid-cols-2 gap-12 md:grid-cols-4">
-        {movies.map((movie, idx) => {
-          if (movies.length === idx + 1) {
-            return (
-              <div
-                ref={lastMovieElementRef} // 마지막 요소에 ref를 연결
-                key={movie.movieNum}
-                className="flex flex-col items-center"
-              >
-                <div className="relative">
-                  <Link to={`/movie/detail/${movie.movieNum}`}>
-                    <img
-                      src={movie.posterUrl}
-                      className="h-96 w-auto cursor-pointer rounded-md object-cover"
-                    />
-                  </Link>
-                  <span className="absolute left-4 top-2 rotate-3 transform text-2xl text-white">
-                    {movie.movieNum}
+        {movie.map((movie, idx) => (
+          <div
+            key={`${movie.movieNum}-${idx}`}
+            className="flex flex-col items-center"
+            ref={movie.length === idx + 1 ? lastMovieElementRef : null}
+          >
+            <div className="relative">
+              <Link to={`/movie/detail/${movie.movieNum}`}>
+                <img
+                  src={`http://localhost:8080/api/admin/movie/view/${movie.posterUrl}`}
+                  alt="Poster"
+                  className="h-96 w-auto cursor-pointer rounded-md object-cover"
+                />
+              </Link>
+            </div>
+            <div className="mt-4 text-center text-2xl text-white">
+              {movie.korTitle}
+            </div>
+            <div className="mt-6 flex w-full flex-col items-start text-left">
+              <div className="flex w-full items-center justify-between">
+                <div className="flex flex-col">
+                  <span className="text-sm text-gray-300">{movie.rating}</span>
+                  <span className="text-sm text-gray-300">
+                    <span>개봉일: </span>
+                    {movie.movieStartDate}
                   </span>
                 </div>
-                <div className="mt-4 text-center text-2xl text-white">
-                  {movie.korTitle}
-                </div>
-                <div className="mt-6 flex w-full flex-col items-start text-left">
-                  <div className="flex w-full items-center justify-between">
-                    <div className="flex flex-col">
-                      <span className="text-sm text-gray-300">
-                        {movie.rating}
-                      </span>
-                      <span className="text-sm text-gray-300">
-                        <span>개봉일: </span>
-                        {movie.movieStartDate}
-                      </span>
-                    </div>
-                    <Link
-                      to={`/reservation/${movie.movieNum}`}
-                      className="ml-4 rounded bg-gray-500 px-4 py-2 text-center text-white"
-                    >
-                      예매하기
-                    </Link>
-                  </div>
-                </div>
+                <Link
+                  to={`/reservation/${movie.movieNum}`}
+                  className="ml-4 rounded bg-emerald-500 px-4 py-2 text-center text-white"
+                >
+                  예매하기
+                </Link>
               </div>
-            );
-          } else {
-            return (
-              <div key={movie.movieNum} className="flex flex-col items-center">
-                <div className="relative">
-                  <Link to={`/movie/detail/${movie.movieNum}`}>
-                    <img
-                      src={movie.posterUrl}
-                      className="h-96 w-auto cursor-pointer rounded-md object-cover"
-                    />
-                  </Link>
-                  <span className="absolute left-4 top-2 rotate-3 transform text-2xl text-white">
-                    {movie.movieNum}
-                  </span>
-                </div>
-                <div className="mt-4 text-center text-2xl text-white">
-                  {movie.korTitle}
-                </div>
-                <div className="mt-6 flex w-full flex-col items-start text-left">
-                  <div className="flex w-full items-center justify-between">
-                    <div className="flex flex-col">
-                      <span className="text-sm text-gray-300">
-                        {movie.rating}
-                      </span>
-                      <span className="text-sm text-gray-300">
-                        <span>개봉일: </span>
-                        {movie.movieStartDate}
-                      </span>
-                    </div>
-                    <Link
-                      to={`/reservation/${movie.movieNum}`}
-                      className="ml-4 rounded bg-gray-500 px-4 py-2 text-center text-white"
-                    >
-                      예매하기
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            );
-          }
-        })}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
