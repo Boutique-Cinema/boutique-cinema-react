3bb5bdb261e5ed2f71ded733c87040ac10fff92e
cfgop23 <cfgop23@gmail.com>
2024-10-28 11:48:08 +0900

    modify : 관리자 헤더 로고 클릭 경로 수정

--- src/components/layouts/AdminHeader.jsx
+++ src/components/layouts/AdminHeader.jsx
@@ -25,7 +25,7 @@ export default function AdminHeader() {
   return (
     <header className="flex h-[11vh] items-end">
       <div className="w-1/3">
-        <a href="/admin/movie/list">
+        <a href="/">
           <Logo />
         </a>
       </div>
